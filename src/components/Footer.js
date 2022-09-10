f847ad864c363c81433849f5959e3e07fd39a095
mat-jar <mtu07@poczta.fm>
2022-09-10 14:09:37 +0200

    Align footer centrally

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer(props) {
   return(
-    <footer className=" position-absolute m-5 start-50 translate-middle">
+    <footer className="m-5 start-50">
     <p style={{textAlign: 'center'}} >© 2022 Mateusz Jarczyński</p>
     </footer>
   );
