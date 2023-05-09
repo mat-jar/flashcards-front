b92b50cd7d837d3693d69a1ed918eef93a2b85ce
mat-jar <mtu07@poczta.fm>
2023-05-09 02:56:32 +0200

    Add shuffling function

--- src/Utils.js
+++ src/Utils.js
@@ -6,3 +6,15 @@ export const displayCorners = (side) => {
     "rounded-" + side + "-1-5"
   )
 };
+
+export function shuffle(input_array) {
+  var array = structuredClone(input_array)
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array
+};
